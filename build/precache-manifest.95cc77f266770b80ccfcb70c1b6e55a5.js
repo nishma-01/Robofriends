b61a34cfd6a21f9a7e38d5da3f70ec715b8d0c06
nishma_mehta <nishmamehta1@gmail.com>
2022-08-30 19:50:27 +0100

    deployed website

---
+++ build/precache-manifest.95cc77f266770b80ccfcb70c1b6e55a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bddcd20ad0f0fce0e13",
+    "url": "/robofriends/static/css/main.9cdf50cd.chunk.css"
+  },
+  {
+    "revision": "4bddcd20ad0f0fce0e13",
+    "url": "/robofriends/static/js/main.4bddcd20.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "fdd26a3ae15dcf4bfeb7",
+    "url": "/robofriends/static/css/2.7b9c22bd.chunk.css"
+  },
+  {
+    "revision": "fdd26a3ae15dcf4bfeb7",
+    "url": "/robofriends/static/js/2.fdd26a3a.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8f63f64691d153c571f19e254f9fc959",
+    "url": "/robofriends/index.html"
+  }
+];
